25882a21e8c1f20030cee3d726285fa96a432b55
Slvr <slvrgauthier@gmail.com>
2014-12-21 18:47:56 +0100

    326

--- EE326/TP-Tracking/ProgrammeMatlab/Compiler.m
+++
@@ -1,4 +0,0 @@
-reponse = which('delete DeriveImage.mexmac') ;
-if(strcmp(reponse,'')==0) delete DeriveImage.mexmac ; end ;
-% mex -v -argcheck DeriveOmni.c % InterNoyau.c Interaction.c Choquet.c Tri.c
-mex -v -argcheck DeriveImage.c Deriche.c Castan.c % Choquet.c 
